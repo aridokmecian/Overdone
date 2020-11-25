33f8c11d20850216ea2a6c51a9fd5631b5c08d64
Ari Dokmecian <aridokmecian@gmail.com>
2020-11-25 02:15:54 -0500

    DEV:  created custom NavBar and added settings sheet

--- overdone/Views/ListView.swift
+++ overdone/Views/ListView.swift
@@ -16,73 +16,88 @@ struct ListView: View {
     
     private var entries: FetchedResults<TodoEntry>
     
-    @State private var showSheet = false
+    @State private var showNewTodoSheet = false
+    @State private var showSettingsSheet = false
     
     @State private var showButton = true
     
     @State private var selection = 0
     var body: some View {
         NavigationView {
-            Group {
-                if (entries.isEmpty) {
-                    VStack(spacing: 16) {
-                        Image("box")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100)
-                        Text("You have no tasks today! Take it easy!")
-                        Button(action: {
-                            self.showSheet = true
-                        }, label: {
-                            Text("Add a task!")
-                        })
-                    }
-                } else {
-                    ScrollView {
-                        Picker("Priority", selection: $selection) {
-                            Text("None").tag(0)
-                                .font(.title)
-                            Text("Low").tag(1)
-                                .font(.title)
-                            Text("High").tag(2)
-                                .font(.title)
-                        }.pickerStyle(SegmentedPickerStyle())
+            VStack {
+                HStack {
+                    Text("Tasks")
+                        .bold()
+                        .font(.title)
                         .padding()
-                        LazyVStack(alignment: .center, pinnedViews: [.sectionFooters]) {
-                            Section(footer: FloatingButtonView(action: {self.showSheet = true}).padding()) {
-                                
-                                ForEach(entries) { entry in
-                                    ListEntry(entry: entry)
+                    
+                    Spacer()
+                    
+                    // Settings Button
+                    Button(action: {
+                        self.showSettingsSheet = true
+                    }) {
+                        Image(systemName: "gearshape.fill")
+                            .labelsHidden()
+                    }
+                    .padding()
+                    .font(.title)
+                    .sheet(isPresented: $showSettingsSheet) {
+                        SettingsView()
+                    }
+                    
+                    // New Task Button
+                    Button(action: {
+                        self.showNewTodoSheet = true
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .padding()
+                    .font(.title)
+                    .sheet(isPresented: $showNewTodoSheet) {
+                        NewTodoPage()
+                    }
+                }
+                Spacer()
+                Group {
+                    if (entries.isEmpty) {
+                        VStack(spacing: 16) {
+                            Image("box")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100)
+                            Text("You have no tasks today! Take it easy!")
+                            Button(action: {
+                                self.showNewTodoSheet = true
+                            }, label: {
+                                Text("Add a task!")
+                            })
+                        }
+                    } else {
+                        ScrollView {
+                            Picker("Priority", selection: $selection) {
+                                Text("None").tag(0)
+                                    .font(.title)
+                                Text("Low").tag(1)
+                                    .font(.title)
+                                Text("High").tag(2)
+                                    .font(.title)
+                            }.pickerStyle(SegmentedPickerStyle())
+                            .padding()
+                            LazyVStack(alignment: .center, pinnedViews: [.sectionFooters]) {
+                                Section(footer: FloatingButtonView(action: {self.showNewTodoSheet = true}).padding()) {
+                                    
+                                    ForEach(entries) { entry in
+                                        ListEntry(entry: entry)
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                Spacer()
             }
-            .navigationTitle("Tasks")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        self.showSheet = true
-                    }) {
-                        Label("Add Item", systemImage: "gearshape.fill")
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        self.showSheet = true
-                    }) {
-                        Label("Add Item", systemImage: "plus.circle.fill")
-                    }
-                }
-            }
-        }
-        .sheet(isPresented: $showSheet) {
-            NewTodoPage()
+            .navigationBarHidden(true)
         }
     }
 }
